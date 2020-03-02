88c5df96bbee0bda8f593217584b41202251c358
VancedOfficial <teamvanced@gmail.com>
2020-03-02 23:42:57 +0200

    Not replacing test

--- buildinfo.sh
+++ buildinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-buildVersion=`cat buildversion.txt`
+buildVersion=`cat buildVersion.txt`
 commitNumber=`cat commitNumber.txt`
 sed -i -e "s/{{buildNumber}}/$buildVersion/g" index.html
 sed -i -e "s/{{commitNumber}}/$commitNumber/g" index.html
