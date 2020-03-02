834313f7a3a4f4d3af3318bf9dc447c607cacb61
VancedOfficial <teamvanced@gmail.com>
2020-03-02 23:51:07 +0200

    Refactoring

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+BUILD_VERSION_PATH="$WEB_PATH/buildVersion.txt"
+COMMIT_NUMBER_PATH="$WEB_PATH/commitNumber.txt"
+
 echo "***********[ Removing IP from logs ]***********"
 ssh $SERVER_USERNAME@$SERVER_NAME "echo Hidden" > /dev/null 2>&1
+
+echo "***********[ Removing the added version files so git pull doesn't fail ]***********"
+ssh $SERVER_USERNAME@$SERVER_NAME "rm -f $BUILD_VERSION_PATH"
+ssh $SERVER_USERNAME@$SERVER_NAME "rm -f $COMMIT_NUMBER_PATH"
+
 echo "***********[ Running the update Docker for the site ]***********"
 ssh $SERVER_USERNAME@$SERVER_NAME "docker run --rm -v $WEB_PATH:/git-project $UPDATE_DOCKER_NAME"
 
-BUILD_VERSION_PATH="$WEB_PATH/buildVersion.txt"
-COMMIT_NUMBER_PATH="$WEB_PATH/commitNumber.txt"
 echo "***********[ Update the build number for the site ]***********"
-ssh $SERVER_USERNAME@$SERVER_NAME "echo $TRAVIS_BUILD_NUMBER > $WEB_PATH/buildVersion.txt"
+ssh $SERVER_USERNAME@$SERVER_NAME "echo $TRAVIS_BUILD_NUMBER > $BUILD_VERSION_PATH"
 ssh $SERVER_USERNAME@$SERVER_NAME "echo $TRAVIS_COMMIT > $COMMIT_NUMBER_PATH"
 ssh $SERVER_USERNAME@$SERVER_NAME ".$WEB_PATH/buildinfo.sh"
-
